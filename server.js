3785c3e3247544d4b3684ab1f7edb03ff10d45c8
raissalst <raissalst@gmail.com>
2022-07-28 19:27:58 -0300

    feat: routes authorizations

--- server.js
+++ server.js
@@ -9,7 +9,22 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 const rules = auth.rewriter({
-  menulist: 444
+  indicebr: 444,
+  indicedf: 444,
+  consolidadobr: 444,
+  consolidadof3: 444,
+  consolidadof5: 444,
+  consolidadof6: 444,
+  consolidadof8: 444,
+  fotocand1: 444,
+  fotocand2: 444,
+  fotocand3: 444,
+  fotocand4: 444,
+  fotocand5: 444,
+  fotocand6: 444,
+  fotocand7: 444,
+  fotocand8: 444,
+  fotocand9: 444
 });
 
 app.use(cors());
